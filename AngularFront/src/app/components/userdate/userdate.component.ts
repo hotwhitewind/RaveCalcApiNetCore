b9b5e1d4b1916639eb5803271c91e72f35adc84c
BM <hotwhitewind@gmail.com>
2020-12-14 17:16:50 +0300

    Change front for clear selected Country, States, Districts and Cities.

--- AngularFront/src/app/components/userdate/userdate.component.ts
+++ AngularFront/src/app/components/userdate/userdate.component.ts
@@ -1,12 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { NgbCalendar, NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { CitiesResponse, City, CountriesResponse, Country, CountryInfoResponse, District, State, StatesResponse } from 'src/app/common/response';
+import { City, CountriesResponse, Country, CountryInfoResponse, District, State } from 'src/app/common/response';
 import { NgbDateCustomParserFormatter } from '../../../filters/dateformat';
 import { FillselectService } from '../../sevices/fillselect.service';
 
@@ -32,14 +28,11 @@ export class UserdateComponent implements OnInit {
   }
 
   countries: string[];
-  states: string[];
-  districts: string[];
-  cities: string[];
-
   currentCountry: Country;
-  currentState: State;
-  currentDistrict: District;
-  currentCity: City;
+
+  currentStates: State[];
+  currentDistricts: District[];
+  currentCities: City[];
 
   constructor(private fb: FormBuilder, private calendar: NgbCalendar, private http: FillselectService) { }
 
@@ -68,58 +61,67 @@ export class UserdateComponent implements OnInit {
         this.onDistrictChanged(f);
       });
 
+    this.dataForm.get("city").valueChanges
+      .subscribe(f => {
+        this.onCityChanged(f);
+      });
+
     of((this.http.getCountries()).subscribe((data: CountriesResponse) => {
       this.countries = data.result;
     }));
   }
 
   setDefalut() {
-    this.currentCountry = null;
-    this.currentState = null;
-    this.currentDistrict = null;
-    this.currentCity = null;
-    this.states = null;
-    this.districts = null;
-    this.cities = null;
-    this.dataForm.get("state").patchValue(null);
-    this.dataForm.get("district").patchValue(null);
-    this.dataForm.get("city").patchValue(null);
+    this.currentCities = null;
+    this.currentStates = null;
+    this.currentDistricts = null;
+    this.dataForm.get('state').patchValue(null);
+    this.dataForm.get('district').patchValue(null);
+    this.dataForm.get('city').patchValue(null);
   }
 
   onCountryChanged(e) {
     this.http.getCountryInfo(e).subscribe((data: CountryInfoResponse) => {
       this.setDefalut();
       this.currentCountry = data.result;
-      if (data.result.states && data.result.states.length > 0)
-        this.states = data.result.states.map(s => s.stateName);
-      if (data.result.cities && data.result.cities.length > 0)
-        this.cities = data.result.cities.map(s => s.cityName);
-    })
+      this.currentStates = data.result?.states;
+      this.currentCities = data.result?.cities;
+    });
   }
 
   onStateChanged(e) {
     console.log(e)
-    if (this.currentCountry && e) {
-      this.currentState = this.currentCountry.states.find(c => c.stateName === e);
-      if (this.currentState) {
-        if (this.currentState.districts && this.currentState.districts.length > 0) {
-          this.districts = this.currentState.districts.map(s => s.districtName);
-        }
-        if (this.currentState.cities && this.currentState.cities.length > 0) {
-          this.cities = this.currentState.cities.map(s => s.cityName);
-        }
+    this.dataForm.get('district').patchValue(null);
+    this.dataForm.get('city').patchValue(null);
+
+    if (e == null) {
+      this.currentDistricts = null;
+      this.currentCities = this.currentCountry?.cities;
+    }
+    else {
+      if (this.currentStates) {
+        this.currentDistricts = this.currentStates.find(c => c.stateName == e)?.districts;
+        this.currentCities = this.currentStates.find(c => c.stateName == e)?.cities;
       }
     }
   }
 
   onDistrictChanged(e) {
     console.log(e)
-    if (this.currentState && e) {
-      if (this.currentState.districts && this.currentState.districts.length > 0)
-        this.currentDistrict = this.currentState.districts.find(c => c.districtName === e);
-      if (this.currentDistrict && this.currentDistrict.cities && this.currentDistrict.cities.length > 0)
-        this.cities = this.currentDistrict.cities.map(s => s.cityName);
+    this.dataForm.get('city').patchValue(null);
+    if (e == null && this.currentStates != null) {
+      let currentStateName = this.dataForm.get('state').value;
+      this.currentCities = this.currentStates.find(c => c.stateName == currentStateName)?.cities;
     }
+    else {
+      if (this.currentDistricts) {
+        this.currentCities = this.currentDistricts.find(c => c.districtName == e)?.cities;
+      }
+    }
+  }
+
+  onCityChanged(e) {
+
   }
 
   onSubmit(form: FormGroup) {
