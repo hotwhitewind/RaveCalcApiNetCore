27872ab7cd1d135d0241a1f9fc2510a078c46354
BM <hotwhitewind@gmail.com>
2020-12-16 12:27:23 +0300

    Add controller for CityInfo and ConvertDate

---
+++ RaveCalcApiCommander/Controllers/CityInfoController.cs
@@ -0,0 +1,235 @@
+﻿using ConvertGeoNamesDBToMongoDB.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RaveCalcApiCommander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeZoneCorrectorLibrary.Abstraction;
+
+namespace RaveCalcApiCommander
+{
+    [Route("apiv2")]
+    [ApiController]
+    public class CityInfoController : ControllerBase
+    {
+        private readonly ILogger<CityInfoController> _logger;
+        private readonly IConfiguration _config;
+        private readonly ITimeZoneCorrector _timeZoneCorrector;
+
+        public CityInfoController(ILogger<CityInfoController> logger, IConfiguration config,
+            ITimeZoneCorrector timeZoneCorrector)
+        {
+            _logger = logger;
+            _config = config;
+            _timeZoneCorrector = timeZoneCorrector;
+        }
+
+        [HttpGet]
+        [Route("getallcountries")]
+        public ActionResult GetAllCountriesName()
+        {
+            var countries = _timeZoneCorrector.GetCountries();
+            if (countries == null)
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = "Countries not found"
+                });
+            }
+            return Ok(new ResponseResult<List<string>>()
+            {
+                error = false,
+                result = countries
+            });
+        }
+
+        [HttpGet]
+        [Route("getcountryinfo")]
+        public ActionResult GetCountryInfo([FromQuery] StatesQuery query)
+        {
+            if (query.countryName == null)
+            {
+                ModelState.AddModelError("countryName", "Отсутствует значение параметра");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = GetModelStateError()
+                });
+            }
+            var country = _timeZoneCorrector.GetCountryInfo(query.countryName);
+            if (country == null)
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = "Country not found"
+                });
+            }
+            return Ok(new ResponseResult<Country>()
+            {
+                error = false,
+                result = country
+            });
+        }
+
+        [HttpGet]
+        [Route("getallstates")]
+        public ActionResult GetAllStates([FromQuery] StatesQuery query)
+        {
+            if (query.countryName == null)
+            {
+                ModelState.AddModelError("countryName", "Отсутствует значение параметра");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = GetModelStateError()
+                });
+            }
+            var states = _timeZoneCorrector.GetStates(query.countryName);
+            if (states == null)
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = "States not found"
+                });
+            }
+            return Ok(new ResponseResult<List<string>>()
+            {
+                error = false,
+                result = states
+            });
+        }
+
+        [HttpGet]
+        [Route("getalldistricts")]
+        public ActionResult GetAllDistrict([FromQuery] DistrictsQuery query)
+        {
+            if (query.countryName == null)
+            {
+                ModelState.AddModelError("countryName", "Отсутствует значение параметра");
+            }
+            if (query.stateName == null)
+            {
+                ModelState.AddModelError("stateName", "Отсутствует значение параметра");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = GetModelStateError()
+                });
+            }
+            var states = _timeZoneCorrector.GetDistricts(query.countryName, query.stateName);
+            if (states == null)
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = "Districts not found"
+                });
+            }
+            return Ok(new ResponseResult<List<string>>()
+            {
+                error = false,
+                result = states
+            });
+        }
+
+        [HttpGet]
+        [Route("getallcities")]
+        public ActionResult GetAllCities([FromQuery] CitiesQuery query)
+        {
+            if (query.countryName == null)
+            {
+                ModelState.AddModelError("countryName", "Отсутствует значение параметра");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = GetModelStateError()
+                });
+            }
+            var cities = _timeZoneCorrector.GetCities(query.countryName, query.stateName, query.districtName);
+            if (cities == null)
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = "Cities not found"
+                });
+            }
+            return Ok(new ResponseResult<List<string>>()
+            {
+                error = false,
+                result = cities
+            });
+        }
+
+        [HttpGet]
+        [Route("getcity")]
+        public ActionResult GetCity([FromQuery] CityQuery query)
+        {
+            if (query.countryName == null)
+            {
+                ModelState.AddModelError("countryName", "Отсутствует значение параметра");
+            }
+            if (query.cityName == null)
+            {
+                ModelState.AddModelError("cityName", "Отсутствует значение параметра");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = GetModelStateError()
+                });
+            }
+            var city = _timeZoneCorrector.GetCity(query.countryName, query.stateName, query.districtName,
+                query.cityName);
+            if (city == null)
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = "City not found"
+                });
+            }
+
+            return Ok(new ResponseResult<City>()
+            {
+                error = false,
+                result = city
+            });
+        }
+
+        public string GetModelStateError()
+        {
+            var query = (from kvp in ModelState
+                         let field = kvp.Key
+                         let state = kvp.Value
+                         where state.Errors.Count > 0
+                         let val = state.AttemptedValue ?? "[NULL]"
+                         let errors = string.Join(";", state.Errors.Select(err => err.ErrorMessage))
+                         select string.Format("{0}:[{1}] (ERRORS: {2})", field, val, errors));
+            return string.Join("  ", query);
+        }
+    }
+}
---
+++ RaveCalcApiCommander/Controllers/ConvertDateController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RaveCalcApiCommander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeZoneCorrectorLibrary.Abstraction;
+
+namespace RaveCalcApiCommander.Controllers
+{
+    [Route("apiv2")]
+    [ApiController]
+    public class ConvertDateController : ControllerBase
+    {
+        private readonly ITimeZoneCorrector _timeZoneCorrector;
+
+        public ConvertDateController(ITimeZoneCorrector timeZoneCorrector)
+        {
+            _timeZoneCorrector = timeZoneCorrector;
+        }
+
+        [HttpGet]
+        [Route("utcdatabytz")]
+        public ActionResult GetUTCDataTimeByTimeZone([FromQuery] DataQuery query)
+        {
+            if (query.timeZoneName == null)
+            {
+                ModelState.AddModelError("timeZoneName", "Отсутствует значение параметра");
+            }
+            if (query.date == null)
+            {
+                ModelState.AddModelError("date", "Отсутствует значение параметра");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = GetModelStateError()
+                });
+            }
+
+            DateTime dateFormat;
+            if (!DateTime.TryParse(query.date, null, System.Globalization.DateTimeStyles.RoundtripKind, out dateFormat))
+            {
+                ModelState.AddModelError("date", "Недопустимый формат даты");
+            }
+            if (_timeZoneCorrector.ConvertToUtcFromCustomTimeZone(query.timeZoneName, dateFormat, out DateTime utcDate))
+            {
+                return Ok(new ResponseResult<DateTime>()
+                {
+                    error = false,
+                    result = utcDate
+                });
+            }
+            else
+            {
+                return BadRequest(new ResponseError
+                {
+                    error = true,
+                    message = "Date not converted"
+                });
+            }
+        }
+        public string GetModelStateError()
+        {
+            var query = (from kvp in ModelState
+                         let field = kvp.Key
+                         let state = kvp.Value
+                         where state.Errors.Count > 0
+                         let val = state.AttemptedValue ?? "[NULL]"
+                         let errors = string.Join(";", state.Errors.Select(err => err.ErrorMessage))
+                         select string.Format("{0}:[{1}] (ERRORS: {2})", field, val, errors));
+            return string.Join("  ", query);
+        }
+    }
+}
